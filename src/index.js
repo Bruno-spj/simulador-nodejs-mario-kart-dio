cb793cd9e61f56d985092ce4f18f6bb6376b662f
Bruno-spj <bruno_spj@hotmail.com>
2025-06-26 19:01:39 -0300

    adicionado arquivo index.js e os objetos de cada jogador

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
+const player1 = {
+  name: "mario",
+  velocidade: 4,
+  manobrabilidade: 3,
+  poder: 3,
+  pontos: 0
+};
+
+const player2 = {
+  name: "bowser",
+  velocidade: 5,
+  manobrabilidade: 2,
+  poder: 5,
+  pontos: 0
+}
+const player3 = {
+  name: "peach",
+  velocidade: 3,
+  manobrabilidade: 4,
+  poder: 2,
+  pontos: 0
+}
+const player4 = {
+  name: "dk",
+  velocidade: 2,
+  manobrabilidade: 2,
+  poder: 5,
+  pontos: 0
+}
+const player5 = {
+  name: "luigi",
+  velocidade: 3,
+  manobrabilidade: 4,
+  poder: 4,
+  pontos: 0
+}
+const player6 = {
+  name: "yoshi",
+  velocidade: 2,
+  manobrabilidade: 4,
+  poder: 3,
+  pontos: 0
+}
+
 
